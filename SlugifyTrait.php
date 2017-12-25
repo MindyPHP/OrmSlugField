ad2215567192c9a5d47329bab7eef3258a7ed3d9
Maxim <max@studio107.ru>
2017-12-25 16:14:53 +0300

    reformat code

--- SlugifyTrait.php
+++ SlugifyTrait.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 /*
- * This file is part of Mindy Framework.
- * (c) 2017 Maxim Falaleev
+ * Studio 107 (c) 2017 Maxim Falaleev
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -26,7 +27,7 @@ trait SlugifyTrait
      */
     protected function createSlug($source)
     {
-        if ($this->slugify === null) {
+        if (null === $this->slugify) {
             $this->slugify = new Slugify();
         }
 
